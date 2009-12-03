9fe3ce844c7e069df4648ca14e0e48f42c3b1e0a
void <git.email.address@googlemail.com>
2009-12-03 20:39:09 +0100

    User registration visuals

--- visual/user.rb
+++ visual/user.rb
@@ -1,37 +1,39 @@
+require 'site/FormWriter'
+
 def visualLoginForm()
 	output =
 <<END
 <p>
-The primary purpose of user accounts on this site is currently offering more convenient access to the pastebin.
-It allows you to edit/delete your old pastebin entries even after your IP has changed.
+The primary purpose of user accounts on this site is currently all about offering more convenient access to the pastebin.
+It allows you to edit/delete your old pastebin entries even after your IP has changed which can be of importance to users with dynamic IPs.
 The login sessions depend on cookies so you will not be able to use this feature unless you enable them in your browser.
 If you do not have an account yet you may register one:
 </p>
-
 <p><a href="registerAccount">Register a new account</a></p>
-
 <p>Specify your username and your password in the following form and submit the data in order to log into your account.</p>
-
-<form action="performLogin" method="post">
-<p>
-<label for="user">User:</label><br />
-<input type="text" id="user" name="user" />
-</p>
-<p>
-<label for="password">Password:</label><br />
-<input type="text" id="password" name="password" />
-</p>
-<p>
-<input type="submit" />
-</p>
-</form>
 END
+
+	form = FormWriter.new(output, 'submitLogin')
+	['User name', 'Password'].each { |label| form.label label }
+	form.finish
+	
 	return output
 end
 
 def visualRegisterForm()
 	output =
 <<END
+<p>
+Fill out the following form and submit the data in order to create a new account.
+It is not necessary to specify an e-mail address but it may be useful to do so in case you forget your password.
+</p>
 END
+	form = FormWriter.new(output, 'submitRegistration')
+	form.label 'User'
+	form.label 'Password'
+	form.label 'Type your password again', name = 'passwordAgain'
+	form.label 'Email address', name = 'email'
+	form.finish
+
 	return output
 end
