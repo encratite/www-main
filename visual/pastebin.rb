341cdd69abcefeed2c5407895427235fa47cba38
void <git.email.address@googlemail.com>
2009-12-29 11:14:53 +0100

    Visually more pleasing for low resolutions now

--- visual/pastebin.rb
+++ visual/pastebin.rb
@@ -10,6 +10,35 @@ require 'configuration/cookie'
 def visualPastebinNewPost
 end
 
+def privacyInfo
+<<END
+Public posts are listed on this site and can be accessed freely by all users by following links or guessing the URLs of posts using their numeric identifiers.
+If you do not wish this post to be visible to strangers you might want to mark this post as "Private".
+This will cause the post not to be listed on this site and users will only be able to access it through a long randomly generated string in its URL.
+This way only the people you show it to will know how to access it.
+END
+end
+
+def expirationInfo
+<<END
+By default, all posts on this site are stored permanently and will not be removed automatically.
+If you do not wish your post to remain online indefinitely you may specify when it will expire.
+Registered users may delete their posts at any time once they are logged in.
+Unregistered users may delete their posts as long as their IP address matches the address they used at the time of the creation of the post.
+END
+end
+
+def unitsInfo
+<<END
+Each post in this pastebin consists of one or multiple units.
+Each one of these units can use a different syntax highlighting mode.
+You can add further units to a post at a later time.
+For example, you might make a post which features a README file in plain text format without any syntax highlighting and a unit containing C++ source code which uses C++ syntax highlighting.
+You may enter a more precise description for the particular unit for this post.
+This is particularly useful if you intend to add further units to this post but you may also just leave it empty.
+END
+end
+
 def visualPastebinForm(request, postDescription = nil, unitDescription = nil, content = nil, highlightingSelectionMode = 0, lastSelection = nil)
 	highlightingGroups =
 	[
@@ -91,35 +120,16 @@ def visualPastebinForm(request, postDescription = nil, unitDescription = nil, co
 				end
 			end
 		end
-
-			
-		writer.p do
-<<END
-Public posts are listed on this site and can be accessed freely by all users by following links or guessing the URLs of posts using their numeric identifiers.
-If you do not wish this post to be visible to strangers you might want to mark this post as "Private".
-This will cause the post not to be listed on this site and users will only be able to access it through a long randomly generated string in its URL.
-This way only the people you show it to will know how to access it.
-END
-		end
 		
 		usePrivate = request.cookies[CookieConfiguration::Private] == '1'
 		
 		privacyOptions =
 		[
-			SelectOption.new('Public', '0', !usePrivate),
-			SelectOption.new('Private', '1', usePrivate)
+			SelectOption.new('Public post', '0', !usePrivate),
+			SelectOption.new('Private post', '1', usePrivate)
 		]
 		
 		writer.select(PastebinForm::PrivatePost,  privacyOptions, {label: 'Privacy options'})
-		
-		writer.p do
-<<END
-By default, all posts on this site are stored permanently and will not be removed automatically.
-If you do not wish your post to remain online indefinitely you may specify when it will expire.
-Registered users may delete their posts at any time once they are logged in.
-Unregistered users may delete their posts as long as their IP address matches the address they used at the time of the creation of the post.
-END
-		end
 		
 		firstOffset = 0
 		
@@ -146,17 +156,6 @@ END
 		end		
 		
 		writer.select(PastebinForm::Expiration, expirationOptions, {label: 'Post expiration'})
-
-		writer.p do
-<<END
-Each post in this pastebin consists of one or multiple units.
-Each one of these units can use a different syntax highlighting mode.
-You can add further units to a post at a later time.
-For example, you might make a post which features a README file in plain text format without any syntax highlighting and a unit containing C++ source code which uses C++ syntax highlighting.
-You may enter a more precise description for the particular unit for this post.
-This is particularly useful if you intend to add further units to this post but you may also just leave it empty.
-END
-		end
 		
 		writer.text('Description of this unit (not necessary)', PastebinForm::UnitDescription, unitDescription)
 		writer.textArea('Paste the content here', PastebinForm::Content, content, {cols: '30', rows: '10'})
