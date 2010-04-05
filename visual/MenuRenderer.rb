d0bd5d4137869de8469264848d5311e7230efb71
void <git.email.address@googlemail.com>
2010-04-05 23:23:29 +0200

    Modified the renderer for the new handler menu interface

--- visual/MenuRenderer.rb
+++ visual/MenuRenderer.rb
@@ -7,17 +7,21 @@ class MenuRenderer
 	end
 
 	def self.renderMenu(request)
-		menuStructure = request.manager.getMenu
+		menu = request.handler.getMenu
 		output = ''
 		writer = HTMLWriter.new output
-		writer.ul(id: 'menu') do
-			menuStructure.each do |item|
-				if item.condition.(request)
-					writer.li do
-						writer.a(href: convertPath(item.path)) { item.description }
+		level = 1
+		menu.each do |menuLevel|
+			writer.ul(id: "menu#{level}") do
+				menuLevel.each do |item|
+					if item.condition.(request)
+						writer.li do
+							writer.a(href: convertPath(item.path)) { item.description }
+						end
 					end
 				end
 			end
+			level += 1
 		end
 		return output
 	end
